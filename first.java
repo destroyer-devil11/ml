bd92db5d2b61831c656fc3a43f22f9e696596a16
destroyer-devil11 <hardikgupta8149@gmail.com>
2025-02-07 16:49:59 +0530

    array and for loop

--- first.java
+++ first.java
@@ -2,7 +2,9 @@ package ml;
 
 public class first {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("hi");
+        int a[] = {1, 2, 3, 4, 5};
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 }
